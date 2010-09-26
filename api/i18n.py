7e591ff772a4a60ad520a590fb64850feeba5a56
Kang Zhang <jobo.zh@gmail.com>
2010-09-26 21:04:40 +0800

    Add i18n support

---
+++ api/i18n.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+i18n.py
+
+Created by Kang Zhang on 2010-09-26.
+Copyright (c) 2010 Shanghai Jiao Tong University. All rights reserved.
+"""
+
+import gettext
+_ = gettext.gettext
