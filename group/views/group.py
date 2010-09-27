800d801dfba1885e0c3b07db2b1be7fdbd5c5435
Kang Zhang <jobo.zh@gmail.com>
2010-09-27 12:13:05 +0800

    Refactor group's views 3

---
+++ group/views/group.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+group.py
+
+Created by Kang Zhang on 2010-09-27.
+Copyright (c) 2010 Shanghai Jiao Tong University. All rights reserved.
+"""
+
+
+import logging
+import settings
+
+from google.appengine.ext import webapp
+from google.appengine.ext.db import djangoforms
+
+from duser.auth import get_current_user
+from api.webapp import login_required, api_enabled
+from api.webapp import check_permission, view_method, PermissionUI
+
+from group.models import *
+
+class GroupForm(djangoforms.ModelForm):
+  class Meta:
+    model = Group
+    fields = ['title', 'introduction']
+
+class GroupUI(PermissionUI):
+  """docstring for GroupUI"""
+  def __init__(self, group):
+    super(GroupUI, self).__init__(group)
+    self.group = group
+  
+  @view_method
+  @check_permission('view', "Not allowed to open the group")
+  def view(self):
+    """docstring for view"""
+    return 'group_display.html', locals()
+  
+  @view_method
+  @check_permission('edit', "Not a admin user")
+  def edit(self):
+    """docstring for edit"""
+    pass
+  
+  @check_permission('edit', "Not a admin user")
+  def edit_post(self, request):
+    """the post_back handler for edit group info"""
+    pass
+  
+  @view_method
+  @check_permission('create_thread', "Not allowed to create thread here")
+  def create_thread(self):
+    """docstring for create_thread"""
+    pass
+    
+  @check_permission('create_thread', "Not allowed to create thread here")
+  def create_thread_post(self, request):
+    """docstring for create_thread_post"""
+    pass
+
+class GroupHandler(webapp.RequestHandler):
+  """docstring for GroupHandler"""
+
+  @api_enabled
+  def get(self, group_id):
+    """docstring for get"""
+    group = Group.get_by_id(int(group_id))
+    return GroupUI(group).view()
