3641ca730d99ca45ee14b1525b40c74a35222817
Kang Zhang <jobo.zh@gmail.com>
2011-02-15 14:34:22 +0800

    Add chrisw module and its base layout

---
+++ chrisw/core/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Kang Zhang on 2011-02-15.
+Copyright (c) 2011 Shanghai Jiao Tong University. All rights reserved.
+"""
+
+
---
+++ chrisw/core/router.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+router.py
+
+Created by Kang Zhang on 2011-02-15.
+Copyright (c) 2011 Shanghai Jiao Tong University. All rights reserved.
+"""
+
+
+
