cea878aa85a4c715f3ae66bb0938704403b43ff6
Kang Zhang <jobo.zh@gmail.com>
2011-03-11 14:02:49 +0800

    finish the Relation and Subscription model

--- common/models.py
+++ common/models.py
@@ -12,18 +12,52 @@ from chrisw import db
 class Entity(db.FlyModel):
   """docstring for Entity"""
   create_at = db.DateTimeProperty(required=True)
-  pass
 
 class Relation(db.Model):
   """docstring for Relation"""
   relation = db.StringProperty(required=True)
-  source = db.StringProperty(required=True)
-  target = db.StringProperty(required=True)
+  source = db.WeakReferenceProperty(required=True)
+  target = db.WeakReferenceProperty(required=True)
+  
+  @classmethod
+  def get_by_relation(cls, relation):
+    """docstring for get_sources_by_relation"""
+    cls.all().filter('relation =', relation)
+  
+  @classmethod
+  def get_by_source(cls, source):
+    """docstring for get_by_source"""
+    cls.all().filter('source =', source)
+  
+  @classmethod
+  def get_by_target(cls, target):
+    """docstring for get_by_target"""
+    cls.all().filter('target =', target)
+  
+  @classmethod
+  def get_by_relation_and_target(cls, relation, target):
+    """docstring for get_source_by_relation"""
+    cls.get_by_relation(relation).filter('target =', target)
+  
+  @classmethod
+  def get_by_source_and_relation(cls, source, relation):
+    """docstring for get_by_source_and_relation"""
+    cls.get_by_relation(relation).filter('source =', source)
   
 class Subscription(db.Model):
   """docstring for Subscription"""
-  subscriber = db.StringProperty(required=True)
-  topic = db.StringProperty(required=True)
+  subscriber = db.WeakReferenceProperty(required=True)
+  topic = db.WeakReferenceProperty(required=True)
+  
+  @classmethod
+  def get_by_topic(cls, topic):
+    """docstring for get_by_topic"""
+    cls.all().filter('topic =', topic)
+  
+  @classmethod
+  def get_by_subscriber(cls, subscriber):
+    """docstring for get_by_subscriber"""
+    cls.all().filter('subscriber =', subscriber)
 
 class Message(db.FlyModel):
   """docstring for Message"""
@@ -32,8 +66,6 @@ class Message(db.FlyModel):
 
 class MessageIndex(db.Model):
   """docstring for MessageIndex"""
-  subscribers = db.StringListProperty(required=True)
+  subscribers = db.ListProperty(db.Key, required=True)
   target = db.ReferenceProperty(required=True)
   create_at = db.DateTimeProperty(auto_now_add=True)
-
-    
